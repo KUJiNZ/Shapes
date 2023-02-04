9fe56e2e84e37595774fca9ccba4236aa8ec0999
Artemis Sheremetiev <deadedante13@gmail.com>
2023-02-04 11:44:31 +0200

    Shapes V1.0.4

--- Tests/test_detect.py
+++ Tests/test_detect.py
@@ -1,6 +1,6 @@
 import ast
 import os
-import pytest as pytest
+import pytest
 
 from Shapes.detect import Detect
 from dotenv import load_dotenv
@@ -8,13 +8,12 @@ from dotenv import load_dotenv
 from Tests.log import Log
 
 # LOGGER
-LOG = Log("__haifaytest__ ", "test_haifa_log.log")
+LOG = Log("__detect__ ", "detect_log.log")
 logger = LOG.logger
+
 
 @pytest.fixture
 def detect():
-
-
     # ENV FILE
     load_dotenv('.env.test')
 
@@ -22,8 +21,8 @@ def detect():
     return Detect()
 
 
-@pytest.mark.is_rectangle
-def is_rectangle(detect):
+@pytest.mark.test_is_rectangle
+def test_is_rectangle(detect):
     """
     Name: Artiom
     Function Name: is_rectangle
@@ -34,7 +33,42 @@ def is_rectangle(detect):
         x = detect.is_rectangle(shape)
         assert x is True
         logger.info(
-            f"{is_rectangle.__doc__}\nActual: {x} Expected: {True}\n")
+            f"{test_is_rectangle.__doc__}\nActual: {x} Expected: {True}\n")
     except Exception as e:
-        logger.exception(f"{is_rectangle.__doc__}{e}\n")
+        logger.exception(f"{test_is_rectangle.__doc__}{e}\n")
+        raise
+
+
+@pytest.mark.test_is_square
+def test_is_square(detect):
+    """
+    Name: Artiom
+    Function Name: is_rectangle
+    Description: Testing if shape is_square
+    """
+    try:
+        shape = ast.literal_eval(os.getenv('SQUARE'))
+        x = detect.is_square(shape)
+        assert x is True
+        logger.info(
+            f"{test_is_square.__doc__}\nActual: {x} Expected: {True}\n")
+    except Exception as e:
+        logger.exception(f"{test_is_square.__doc__}{e}\n")
+        raise
+
+@pytest.mark.test_is_triangle
+def test_is_triangle(detect):
+    """
+    Name: Artiom
+    Function Name: is_triangle
+    Description: Testing if shape is_square
+    """
+    try:
+        shape = ast.literal_eval(os.getenv('TRIANGLE'))
+        x = detect.is_triangle(shape)
+        assert x is True
+        logger.info(
+            f"{test_is_triangle.__doc__}\nActual: {x} Expected: {True}\n")
+    except Exception as e:
+        logger.exception(f"{test_is_triangle.__doc__}{e}\n")
         raise
